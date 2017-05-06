948aadaffd049fb21e47b0eb0ae6ac8f8c7299d5
David  Sosa Valdes <david.sosa.valdes@gmail.com>
2017-05-06 18:34:25 -0500

    Add a simple test environment and a test script

---
+++ tests/unit/NoteCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+use Craftsman\Commands\Notes;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class NoteCommandTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNotMarked()
+    {
+        $app = new Application();
+        $app->add(new Notes);
+
+        $command = $app->find('notes');
+
+        $tester = new CommandTester($command);
+
+        $tester->execute(array(
+            'command' => $command->getName(),
+        ));
+
+        $output = trim(preg_replace('/\s\s+/', ' ', $tester->getDisplay()));
+
+        $this->assertRegexp('/There are not notes marked./', $output);
+    }
+}
